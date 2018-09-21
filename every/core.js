fa62c83f4a3ce24529e52cf69db258d70c0be9ed
Celia Marshall <celia.m@gmail.com>
2018-09-21 10:17:13 -0700

    every tests are passing

--- every/core.js
+++ every/core.js
@@ -2,14 +2,14 @@
 // are even numbers.
 
 function allEven (input) {
-  return input;
+  return input.every(number => number % 2 === 0);
 };
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType (input) {
-  return input;
+  return input.every(element => typeof element === typeof input[0]);
 };
 
 // Check to see if every element in the matrix is
@@ -17,14 +17,17 @@ function allSameType (input) {
 // greater than 0.
 
 function positiveMatrix (input) {
-  return input;
+  return input.every(element => typeof element === 'object' && element.every(number => number > 0));
 };
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels (input) {
-  return input;
+  return input.every(item => {
+    const vowels = item.match(/[aeiouAEIOU]/g)
+    return typeof item === 'string' && vowels.every(vowel => vowel === vowels[0])
+  })
 };
 
 module.exports = {
